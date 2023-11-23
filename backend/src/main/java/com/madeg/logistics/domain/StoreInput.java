b3f619f589d08ed2698ac479a0c45edc5a302ca3
Zinna <choiyh11097@gmail.com>
2023-11-23 22:02:03 +0900

    [fix] Add constraint to StoreInput

--- backend/src/main/java/com/madeg/logistics/domain/StoreInput.java
+++ backend/src/main/java/com/madeg/logistics/domain/StoreInput.java
@@ -2,6 +2,8 @@ package com.madeg.logistics.domain;
 
 import com.madeg.logistics.enums.StoreType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -29,9 +31,15 @@ public class StoreInput {
   @Schema(example = "FIX")
   private StoreType type;
 
+  @DecimalMin(value = "0", message = "고정 비용은 0 미만이 될 수 없습니다.")
   @Schema(example = "30000")
   private Integer fixedCost;
 
+  @DecimalMin(value = "0.0", message = "수수료율은 0.0 미만이 될 수 없습니다.")
+  @DecimalMax(
+    value = "100.0",
+    message = "수수료율은 100.0을 초과할 수 없습니다."
+  )
   @Schema(example = "3.5")
   private Double commissionRate;
 
