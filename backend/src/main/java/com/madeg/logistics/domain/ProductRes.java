6405e18d503373e09122fda1d62ce51b64cf3f1d
Zinna <choiyh11097@gmail.com>
2023-12-21 21:48:22 +0900

    [refactor] SimplePageInfo class has been separated for the common use

--- backend/src/main/java/com/madeg/logistics/domain/ProductRes.java
+++ backend/src/main/java/com/madeg/logistics/domain/ProductRes.java
@@ -12,17 +12,6 @@ public class ProductRes extends CommonRes {
   private List<Product> content;
   private SimplePageInfo pageable;
 
-  @Getter
-  @Setter
-  public static class SimplePageInfo {
-
-    private boolean last;
-    private int page;
-    private int size;
-    private int totalPages;
-    private long totalElements;
-  }
-
   public ProductRes(
     int status,
     String message,
