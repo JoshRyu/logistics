baf93c26bd2c6f122dc8981ef334d26076da2417
Zinna <choiyh11097@gmail.com>
2024-01-02 11:25:58 +0900

    [fix] Add Frontend path to security

--- backend/src/main/java/com/madeg/logistics/config/SecurityConfig.java
+++ backend/src/main/java/com/madeg/logistics/config/SecurityConfig.java
@@ -50,6 +50,16 @@ public class SecurityConfig {
           .permitAll()
           .requestMatchers(CorsUtils::isPreFlightRequest)
           .permitAll()
+          // Allow Frontend Relevant Path
+          .requestMatchers(
+            "/",
+            "/index.html",
+            "/static/**",
+            "/resources/**",
+            "/assets/**",
+            "/favicon.ico"
+          )
+          .permitAll()
           // Allow Swagger Relevant Path
           .requestMatchers(HttpMethod.GET, "/swagger-ui/*")
           .permitAll()
