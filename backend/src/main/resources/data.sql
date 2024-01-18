7f559a1fbd11a9cba64943ae01564a0e3fdacfcd
Zinna <choiyh11097@gmail.com>
2024-01-18 16:42:11 +0900

    [fix] Change in the method of initial user setup: from using data.sql to using @PostConstruct

--- backend/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO member (id, username, password, role)
-VALUES (
-        1,
-        'admin',
-        '{bcrypt}$2a$10$DN7bk6emH.YmUdWF6P08FeprVSDh/L1PnYrawj5XrQaTpCFZblQae',
-        'ADMIN'
-    ) ON CONFLICT (id) DO NOTHING;
