02fea1f40202116eb863f8681bccc87fb01af472
Zinna <choiyh11097@gmail.com>
2024-03-05 11:38:34 +0900

    [rename] rename controller

--- backend/src/main/java/com/madeg/logistics/controller/ViewController.java
+++ backend/src/main/java/com/madeg/logistics/controller/ViewController.java
@@ -5,18 +5,18 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ViewControllers implements WebMvcConfigurer {
+public class ViewController implements WebMvcConfigurer {
 
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
     registry.addViewController("/user/**").setViewName("forward:/index.html");
 
     registry
-      .addViewController("/product/**")
-      .setViewName("forward:/index.html");
+        .addViewController("/product/**")
+        .setViewName("forward:/index.html");
 
     registry
-      .addViewController("/{path:[^\\.]*}")
-      .setViewName("forward:/index.html");
+        .addViewController("/{path:[^\\.]*}")
+        .setViewName("forward:/index.html");
   }
 }
